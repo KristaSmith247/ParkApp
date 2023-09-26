eecad3e2d89f1adebec364afccaf6b370f90858b
KristaSmith247 <118084776+KristaSmith247@users.noreply.github.com>
2023-09-25 20:21:58 -0600

    Added Park.cs and ParkApp.cs

---
+++ Park.cs
@@ -0,0 +1,115 @@
+﻿/* Park.cs
+ * Author: Krista Smith
+ * Date: 9/20/23
+ * Description: The file contains the definition of the Park class.
+ * */
+
+namespace ParkApp
+{
+    public class Park
+    {
+        // properties
+        public string ParkName { get; }
+        public string Location { get; }
+        public string ParkType { get; }
+        public string? Facilities { get; set; }
+        public decimal Fees { get; set; }
+        public decimal NumEmployees { get; set; }
+        public decimal NumVisitors { get; set; }
+        public decimal Budget  { get; set; }
+
+        public decimal Cost { get; set;  }
+
+        public decimal Revenue { get; set;  }
+
+        // constructors
+
+        public Park(string name, string location, string type)
+        {
+            ParkName = name;
+            Location = location;
+            ParkType = type;
+        }
+
+        public Park(string name, string location, string type, string facilities)
+        {
+            ParkName = name;
+            Location = location;
+            ParkType = type;
+            Facilities = facilities;
+        }
+
+        public Park(string name, string location, string type, string facilities, decimal fees, decimal visitors, decimal employees)
+        {
+            ParkName = name;
+            Location = location;
+            ParkType = type;
+            Facilities = facilities;
+            Fees = fees;
+            NumVisitors = visitors;
+            NumEmployees = employees;
+        }
+        public decimal CalculateCost(decimal NumVisitors, decimal Budget)
+        {
+            /* calculate cost per visitor based on annual budget and number
+             * of visitors */
+            decimal budget = Budget;
+            decimal visitors = NumVisitors;
+            return budget / visitors;
+        }
+
+        public decimal CalculateRevenue(decimal NumVisitors, decimal Fees)
+        {
+            /* calculate revenue based on fees per visitor multiplied by 
+             * number of visitors
+             */
+            decimal fees = Fees;
+            decimal visitors = NumVisitors;
+            return fees * visitors;
+        }
+
+
+
+        public string threeVariables(string name, string location, string type)
+        {
+            /* method that returns a string representing the name of the park,
+            / the location, type of park
+            */
+
+            string ParkName = name;
+            string Location = location;
+            string ParkType = type;
+
+            return "\nPark name: " + ParkName + "\nLocation: " +
+                 Location + "\nType of Park: " + ParkType;
+
+        }
+        public string fourVariables(string name, string location, string type, string facilities)
+        {
+            /* method that returns a string representing the name of the park,
+            / the location, type of park, and facilities available
+            */
+
+            string ParkName = name;
+            string Location = location;
+            string ParkType = type;
+            string Facilities = facilities;
+            return "\nPark name: " + ParkName + "\nLocation: " +
+                 Location + "\nType of Park: " + ParkType + "\nFacilities: "
+                 + Facilities;
+        }
+
+        // Output
+        public override string ToString()
+        {
+            // this will return all data members with labels
+            return "\nPark Name: " + ParkName + "\nLocation: " + Location +
+                "\nPark Type: " + ParkType + "\nAvailable facilities: " +
+                Facilities + "\nFees: " + Fees.ToString("C") + "\nNumber of " +
+                "employees: " + NumEmployees.ToString("N") + "\nVisitors: " 
+                + NumVisitors.ToString("N") + "\nAnnual Budget: " + 
+                Budget.ToString("C") + "\nCost per visitor: " + 
+                Cost.ToString("C") + "\nRevenue: " + Revenue.ToString("C");
+        }
+    }
+}
---
+++ ParkApp.cs
@@ -0,0 +1,137 @@
+﻿/* ParkApp.cs
+ * Author: Krista Smith
+ * Date: 9/20/23
+ * Description: This class constructs an object of the Park class. The file includes test cases for the class.
+ *  */
+
+using static System.Console;
+
+namespace ParkApp
+{
+    public class ParkApp
+    {
+        static void Main()
+        {
+            WriteLine("Information about parks that have already been entered: ");
+            // (1) testing with only name, location, type of park given
+            Park Yulie = new Park("Yulie", "Wyoming", "National");
+            WriteLine(Yulie.threeVariables(Yulie.ParkName, Yulie.Location, Yulie.ParkType));
+
+            // (2) testing with name, location, type of park and facilities given
+            Park Heugonot = new Park("Heugonot", "Kerry GA", "State", "Hiking, Biking, Camping");
+            WriteLine(Heugonot.fourVariables(Heugonot.ParkName, Heugonot.Location, Heugonot.ParkType, Heugonot.Facilities));
+
+            // (test case 5: another example of computing cost and revenue)
+            Park Yulie2 = new Park("Yulie", "Wyoming", "National", "Camping", 5, 100000, 500);
+            Yulie2.Budget = 750000;
+            WriteLine(Yulie2);
+            WriteLine("Cost per visitor: {0:C}", Yulie2.CalculateCost(Yulie2.NumVisitors, Yulie2.Budget));
+            WriteLine("Revenue: {0:C}", Yulie2.CalculateRevenue(Yulie2.NumVisitors, Yulie2.Fees));
+
+            // second test case to only print name, location, type of park
+            Park Arches = new Park("Arches", "Utah", "National");
+            WriteLine(Arches.threeVariables(Arches.ParkName, Arches.Location, Arches.ParkType));
+
+            // third test case to only print name, location, type of park
+            Park Bryce = new Park("Bryce Canyon", "Moab, UT", "National");
+            WriteLine(Bryce.threeVariables(Bryce.ParkName, Bryce.Location, Bryce.ParkType));
+
+            // second case to test 4 variables - name, location, park type, and facilities
+            Park Albion = new Park("Albion", "Northern Utah", "State", "Hiking, Camping");
+            WriteLine(Albion.fourVariables(Albion.ParkName, Albion.Location, Albion.ParkType, Albion.Facilities));
+
+            // third case to test 4 variables - name, location, park type, and facilities
+            Park Yellowstone = new Park("Yellowstone", "WY", "National", "Hiking, Camping");
+            WriteLine(Yellowstone.fourVariables(Yellowstone.ParkName, Yellowstone.Location, Yellowstone.ParkType, Yellowstone.Facilities));
+
+            // clear screen
+            WriteLine();
+            WriteLine("Press any key to continue.");
+            ReadKey();
+            Clear();
+
+            // (3) testing with only name, location, type of park and facilities to start
+            Park Hayworth = new Park("Hayworth", "Nebraska", "Local", "No facilities");
+            WriteLine("Input information for Hayworth park: ");
+            try
+            {
+                Hayworth.NumVisitors = GetVisitors();
+                Hayworth.NumEmployees = GetEmployees();
+                Hayworth.Budget = GetBudget();
+                Hayworth.Cost = Hayworth.CalculateCost(Hayworth.NumVisitors, Hayworth.Budget);
+                Hayworth.Fees = GetFees();
+                Hayworth.Revenue = Hayworth.CalculateRevenue(Hayworth.NumVisitors, Hayworth.Fees);
+                WriteLine(Hayworth);
+
+                // clear screen
+                WriteLine("Press any key to continue.");
+                ReadKey();
+                Clear();
+
+                // test case for accepting visitors, employees, budget from user
+                WriteLine("Input the requested information for Central Park: ");
+                Park Central = new Park("Central", "New York", "Local");
+                Central.NumVisitors = GetVisitors();
+                Central.NumEmployees = GetEmployees();
+                Central.Budget = GetBudget();
+                Central.Fees = GetFees();
+                Central.Cost = Central.CalculateCost(Central.NumVisitors, Central.Budget);
+                Central.Revenue = Central.CalculateRevenue(Central.NumVisitors, Central.Fees);
+
+                // clear screen
+                WriteLine("Press any key to continue.");
+                ReadKey();
+                Clear();
+
+
+                // display information for park object
+                WriteLine(Central);
+            }
+            catch (Exception)
+            {
+                WriteLine("Error: Cannot parse input.");
+            }
+        }
+
+        static decimal GetVisitors()
+        {
+            // method that asks for amount of visitors
+            WriteLine("Number of visitors last year: ");
+            string inVal = ReadLine();
+            decimal NumVisitors = decimal.Parse(inVal);
+            return NumVisitors;
+        }
+
+        static decimal GetEmployees()
+        {
+            // method that asks for number of employees
+            WriteLine("Number of employees: ");
+            string inVal = ReadLine();
+            decimal NumEmployees = decimal.Parse(inVal);
+            return NumEmployees;
+        }
+
+        static decimal GetBudget()
+        {
+            // method that asks for annual budget
+
+            Write("Annual budget: ");
+            string inVal = ReadLine();
+            decimal Budget = decimal.Parse(inVal);
+            return Budget;
+
+        }
+
+        static decimal GetFees()
+        {
+            // method that asks for fees
+            WriteLine("Required fees: ");
+            string inVal = ReadLine();
+            decimal fees = decimal.Parse(inVal);
+            return fees;
+        }
+
+
+
+    }
+}
